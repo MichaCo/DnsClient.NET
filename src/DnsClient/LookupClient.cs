4035862c823ea8b8d31f48000510a9e4e12c803f
MichaC <kletterer@web.de>
2016-12-31 17:11:05 +0100

    Readme correction

--- src/DnsClient/LookupClient.cs
+++ src/DnsClient/LookupClient.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using DnsClient.Protocol;
 using DnsClient.Protocol.Options;
 
 namespace DnsClient
