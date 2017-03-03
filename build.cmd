49f990b62439b41cb739c3b899966bacace7f289
MichaC <kletterer@web.de>
2017-03-03 13:02:19 +0100

    Version up and build cleanup

--- build.cmd
+++
@@ -1,3 +0,0 @@
-SET CONFIGURATION=Release
-@ECHO OFF
-PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%~dp0build.ps1' %*; exit $LASTEXITCODE"
