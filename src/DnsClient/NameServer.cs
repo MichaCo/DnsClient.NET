181f2eac1f4995ec71623e96a008237f5bfeabcc
RatherLogical <39105734+RatherLogical@users.noreply.github.com>
2024-02-21 21:43:57 +0000

    Reflection-free compatibility changes

--- src/DnsClient/NameServer.cs
+++ src/DnsClient/NameServer.cs
@@ -246,7 +246,7 @@ namespace DnsClient
 
             var exceptions = new List<Exception>();
 
-            var logger = Logging.LoggerFactory?.CreateLogger(typeof(NameServer).FullName);
+            var logger = Logging.LoggerFactory?.CreateLogger("DnsClient.NameServer");
 
             logger?.LogDebug("Starting to resolve NameServers, skipIPv6SiteLocal:{0}.", skipIPv6SiteLocal);
             try
